018fbbf0a4f06c0cba96ced0dff12d0a182a9601
Elliot Björnell <elliotbjornell@hotmail.com>
2018-04-19 12:20:36 +0200

    start

---
+++ Zephyr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Zephyr.MvcApplication" Language="C#" %>
---
+++ Zephyr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zephyr.Startup))]
+namespace Zephyr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
